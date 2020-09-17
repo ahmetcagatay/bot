95397fcbdd29377d7dded98d95d65aa873998e82
Berkayeren Emin <birtutamesc@gmail.com>
2020-09-17 17:15:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,5 +36,11 @@ async def on_message(message): # Mesajlaşma sırasında yapılacaklar belirtild
         await wiki.resarch(mesaj, message)
     if "!tdk" in mesaj:
         await turkdil.tdkara(mesaj, message)
+    
+    if "!help" == mesaj:
+        embed=discord.Embed(title=Yardım..., description="!komutlar ile sahip olduğum komutlar listesine bakabilirsiniz.", color=0xdd2c2c)
+        embed.set_author(name="Enigma", url="https://www.google.com.tr", icon_url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F8%2F80%2FWikipedia-logo-v2.svg%2F1200px-Wikipedia-logo-v2.svg.png&f=1&nofb=1")
+        embed.set_footer(text="!komutlar yazarak komut listesine ulaşabilirsiniz.")
+        await message.channel.send(embed=embed)
 
 bot.run(to+ke+n)
