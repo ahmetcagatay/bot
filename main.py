7125c7643b94b5e866f6c911395a381083e569c0
Berkayeren Emin <birtutamesc@gmail.com>
2020-09-17 17:32:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,25 @@ async def on_message(message): # Mesajlaşma sırasında yapılacaklar belirtild
     if "!tdk" in mesaj:
         await turkdil.tdkara(mesaj, message)
     
-    if "!help" or "!yardım"== mesaj:
-        embed=discord.Embed(title=Yardım..., description="!komutlar ile sahip olduğum komutlar listesine bakabilirsiniz.", color=0xdd2c2c)
-        embed.set_author(name="Enigma", url="https://www.google.com.tr", icon_url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F8%2F80%2FWikipedia-logo-v2.svg%2F1200px-Wikipedia-logo-v2.svg.png&f=1&nofb=1")
-        embed.set_footer(text="Enigma; Python ile yazılmış, açık kaynaklı bir discord botu")
+    if "!help" == mesaj:
+        embed=discord.Embed(title="Yardım / Help", description="!komutlar ile sahip olduğum özellikleri tek bir listede görüntüleyebilirsin.")
+        embed.set_author(name="Enigma")
+        embed.add_field(name="Dil:", value="%100 Türkçe", inline=False)
+        embed.add_field(name="Yazılım:", value="Açık Kaynak (Github)", inline=False)
+        embed.add_field(name="Yüklenmiş Özellik Sayısı:", value="5", inline=False)
+        embed.add_field(name="Bulunan Sunucu Sayısı:", value="4", inline=True)
+        embed.set_footer(text="Enigma, Python dili ile yazılmış açık kaynaklı bir Discord botudur.")
         await message.channel.send(embed=embed)
+
+    if "!yardım" == mesaj:
+        embed=discord.Embed(title="Yardım / Help", description="!komutlar ile sahip olduğum özellikleri tek bir listede görüntüleyebilirsin.")
+        embed.set_author(name="Enigma")
+        embed.add_field(name="Dil:", value="%100 Türkçe", inline=False)
+        embed.add_field(name="Yazılım:", value="Açık Kaynak (Github)", inline=False)
+        embed.add_field(name="Yüklenmiş Özellik Sayısı:", value="5", inline=False)
+        embed.add_field(name="Bulunan Sunucu Sayısı:", value="4", inline=True)
+        embed.set_footer(text="Enigma, Python dili ile yazılmış açık kaynaklı bir Discord botudur.")
+        await message.channel.send(embed=embed)
+        
 
 bot.run(to+ke+n)
