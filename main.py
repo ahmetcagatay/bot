5bd788fd11472155247400a1cff57cc1df672e9f
Berkayeren Emin <birtutamesc@gmail.com>
2020-09-17 17:17:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def on_message(message): # Mesajlaşma sırasında yapılacaklar belirtild
     if "!help" or "!yardım"== mesaj:
         embed=discord.Embed(title=Yardım..., description="!komutlar ile sahip olduğum komutlar listesine bakabilirsiniz.", color=0xdd2c2c)
         embed.set_author(name="Enigma", url="https://www.google.com.tr", icon_url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F8%2F80%2FWikipedia-logo-v2.svg%2F1200px-Wikipedia-logo-v2.svg.png&f=1&nofb=1")
-        embed.set_footer(text="!komutlar yazarak komut listesine ulaşabilirsiniz.")
+        embed.set_footer(text="Enigma; Python ile yazılmış, açık kaynaklı bir discord botu")
         await message.channel.send(embed=embed)
 
 bot.run(to+ke+n)
